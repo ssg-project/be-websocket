67c61137f3bbfcbd0e74aea44530116d625cbefe
kuni4210 <kuni4210@naver.com>
2024-12-06 13:57:13 +0900

    databse 연결 수정

--- utils/database.py
+++ utils/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost/storage_project"
+load_dotenv()
+DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
